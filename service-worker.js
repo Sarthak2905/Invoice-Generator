bbc2eb8bae0ebb321480a90b1f567ce831db1536
Sarthak2905 <sarthakhendre61@gmail.com>
2025-11-05 21:19:39 +0530

    update service-worker code

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,11 @@
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("invoice-cache").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/invoice.html",
-        "/styles.css",
-        "/script.js",
-        "/manifest.json",
-      ]);
-    })
-  );
+  console.log("Service Worker: Installed");
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker: Activated");
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
